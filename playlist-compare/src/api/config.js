56d16c6ec74b5385e6f7e1b37a2d965c91d5b8b2
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-05-29 11:36:36 +0800

    update api

--- playlist-compare/src/api/config.js
+++ playlist-compare/src/api/config.js
@@ -1,22 +1,19 @@
 export const APIurl = {
-    ncm: "https://autumnfish.cn",
-    qqm:
-        "https://service-c41ch70i-1257843962.gz.apigw.tencentcs.com/release/json_proxy/https://c.y.qq.com/v8/fcg-bin/fcg_v8_playlist_cp.fcg?id=",
+  ncm: "https://ncmapi.v2nd.com",
+  qqm:
+    "https://service-c41ch70i-1257843962.gz.apigw.tencentcs.com/release/json_proxy/https://c.y.qq.com/v8/fcg-bin/fcg_v8_playlist_cp.fcg?id=",
 };
 export function check() {
-    if (!localStorage.getItem("ncm_api_server")) {
-        localStorage.setItem("ncm_api_server", APIurl.ncm);
-        localStorage.setItem("qqm_api_server", APIurl.qqm);
-    }
-    console.log(localStorage.getItem("ncm_api_server"));
-    console.log(localStorage.getItem("qqm_api_server"));
+  if (!localStorage.getItem("ncm_api_server")) {
+    localStorage.setItem("ncm_api_server", APIurl.ncm);
+    localStorage.setItem("qqm_api_server", APIurl.qqm);
+  }
+  console.log(localStorage.getItem("ncm_api_server"));
+  console.log(localStorage.getItem("qqm_api_server"));
 }
 export function reset() {
-    localStorage.setItem("ncm_api_server", "https://autumnfish.cn");
-    localStorage.setItem(
-        "qqm_api_server",
-        "https://service-c41ch70i-1257843962.gz.apigw.tencentcs.com/release/json_proxy/https://c.y.qq.com/v8/fcg-bin/fcg_v8_playlist_cp.fcg?id="
-    );
-    APIurl.qqm = localStorage.getItem("qqm_api_server");
-    APIurl.ncm = localStorage.getItem("ncm_api_server");
+  localStorage.setItem("ncm_api_server", APIurl.ncm);
+  localStorage.setItem("qqm_api_server", APIurl.qqm);
+  APIurl.qqm = localStorage.getItem("qqm_api_server");
+  APIurl.ncm = localStorage.getItem("ncm_api_server");
 }
