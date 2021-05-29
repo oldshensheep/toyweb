6e0ad1e67769a11bebb0e0c70157d148b96d9592
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-05-29 11:03:42 +0800

    f

--- deploy.sh
+++ deploy.sh
@@ -14,16 +14,3 @@ yarn install
 yarn run build
 mv dist plscp
 cp plscp ../dist -r
-
-# cd 到构建输出的目录下 
-cd ../dist
-
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git config --local user.name "github-actions[bot]"
-
-git init
-git add -A
-git commit -m 'deploy'
