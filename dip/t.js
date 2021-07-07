17d3c639cdad51113a980e7014fff4f7c4879f15
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-07-07 14:23:26 +0800

    完成图像卷积的算法

--- dip/t.js
+++ dip/t.js
@@ -1,3 +1,8 @@
+/**
+ * Turn image to a grayscale image.
+ * @param {ImageData} imageData
+ * @returns
+ */
 function grayscale(imageData) {
   let newImageData = new ImageData(imageData.width, imageData.height);
   for (let i = 0; i < newImageData.data.length; i += 4) {
@@ -10,6 +15,11 @@ function grayscale(imageData) {
   }
   return newImageData;
 }
+/**
+ * Invert image color.
+ * @param {ImageData} imageData
+ * @returns {ImageData}
+ */
 function invert(imageData) {
   let newImageData = new ImageData(imageData.width, imageData.height);
   for (let i = 0; i < newImageData.data.length; i += 4) {
@@ -20,6 +30,12 @@ function invert(imageData) {
   }
   return newImageData;
 }
+/**
+ * Image quantification.
+ * @param {ImageData} imageData
+ * @param {Number} q
+ * @returns
+ */
 function quantify(imageData, q) {
   let newImageData = new ImageData(imageData.width, imageData.height);
   for (let i = 0; i < newImageData.data.length; i += 4) {
@@ -30,77 +46,60 @@ function quantify(imageData, q) {
   }
   return newImageData;
 }
-
-function uniform_array(len, value) {
-  let arr = new Array(len);
-  for (let i = 0; i < len; ++i)
-    arr[i] = Array.isArray(value) ? [...value] : value;
-  return arr;
-}
-
-function conv_2d(array, kernel) {
-  let result = uniform_array(array.length, uniform_array(array[0].length, 0));
+/**
+ * Image convolution.
+ * @param {ImageData} imageData
+ * @param {Array<Number>} kernel 2D Array
+ */
+function conv_imageData(imageData, kernel) {
+  let newImageData = new ImageData(imageData.width, imageData.height);
+  let iHeight = imageData.height;
+  let iWidth = imageData.width;
   let kRows = kernel.length;
   let kCols = kernel[0].length;
-  let rows = array.length;
-  let cols = array[0].length;
   let kCenterX = Math.floor(kCols / 2);
   let kCenterY = Math.floor(kRows / 2);
-  let i, j, m, n, ii, jj;
-
-  for (i = 0; i < rows; ++i) {
-    for (j = 0; j < cols; ++j) {
-      for (m = 0; m < kRows; ++m) {
-        for (n = 0; n < kCols; ++n) {
-          ii = i + (m - kCenterY);
-          jj = j + (n - kCenterX);
-          if (ii >= 0 && ii < rows && jj >= 0 && jj < cols) {
-            result[i][j] += array[ii][jj] * kernel[m][n];
+  for (let i = 0; i < iHeight; i++) {
+    for (let j = 0; j < iWidth; j++) {
+      newImageData.data[(i * iWidth + j) * 4 + 3] =
+        imageData.data[(i * iWidth + j) * 4 + 3];
+      for (let m = 0; m < kRows; m++) {
+        for (let n = 0; n < kCols; n++) {
+          let ii = i + (m - kCenterY);
+          let jj = j + (n - kCenterX);
+          if (ii >= 0 && ii < iHeight && jj >= 0 && jj < iWidth) {
+            newImageData.data[(i * iWidth + j) * 4 + 0] +=
+              imageData.data[(ii * iWidth + jj) * 4 + 0] * kernel[m][n];
+            newImageData.data[(i * iWidth + j) * 4 + 1] +=
+              imageData.data[(ii * iWidth + jj) * 4 + 1] * kernel[m][n];
+            newImageData.data[(i * iWidth + j) * 4 + 2] +=
+              imageData.data[(ii * iWidth + jj) * 4 + 2] * kernel[m][n];
           }
         }
       }
     }
   }
-  return result;
+  return newImageData;
 }
-
-function unflatten(imageU8CA, w, h) {
+/**
+ * Turn imageData to an 2D array
+ * @param {ImageData} imageData
+ * @returns
+ */
+function unflatten(imageData) {
   let ia = [];
-  for (let i = 0; i < w; i++) {
+  let iHeight = imageData.height;
+  let iWidth = imageData.width;
+  for (let i = 0; i < iWidth; i++) {
     ia[i] = [];
-    for (let j = 0; j < h; j++) {
+    for (let j = 0; j < iHeight; j++) {
       ia[i][j] = [
-        imageU8CA[i * j + 0],
-        imageU8CA[i * j + 1],
-        imageU8CA[i * j + 2],
-        imageU8CA[i * j + 3],
+        imageData.data[(j * iWidth + i) * 4 + 0],
+        imageData.data[(j * iWidth + i) * 4 + 1],
+        imageData.data[(j * iWidth + i) * 4 + 2],
+        imageData.data[(j * iWidth + i) * 4 + 3],
       ];
     }
   }
   return ia;
 }
-
-function flatten(image) {
-  return null;
-}
-
-// let a = [
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-//   [1, 2, 3, 4, 5, 6, 7, 8, 9],
-// ];
-
-// let b = [
-//   [0, 0, 0],
-//   [0, 0, 0],
-//   [0, 0, 0],
-// ];
-
-// conv_2d(b, a).forEach((row) => console.log(row.join(" ")));
-
