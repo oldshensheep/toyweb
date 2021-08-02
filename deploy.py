d88c358cf6c94d81f96f62ade3845940f059e617
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-08-02 20:52:04 +0800

    update build script

---
+++ deploy.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+exclude_dirs = ['playlist-compare', '.github',
+                '.git', '.idea', 'deploy.py', '.gitignore']
+
+for root, dirs, files in os.walk('./'):
+    files = set(dirs+files) - set(exclude_dirs)
+    print(files)
+    os.system('cd playlist-compare && yarn install && yarn run build')
+    os.system('cd playlist-compare && mv dist plscp && cp plscp ../dist - r')
+    for f in files:
+        shutil.move(f, 'dist')
+    break
