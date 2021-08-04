84428fcf6ceda914f12fe8a0a031831323b593da
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-08-04 21:31:48 +0800

    update deploy.py

--- deploy.py
+++ deploy.py
@@ -9,7 +9,7 @@ for root, dirs, files in os.walk('./'):
     files = set(dirs+files) - set(exclude_dirs)
     print(files)
     os.system('cd playlist-compare && yarn install && yarn run build')
-    os.system('cd playlist-compare && mv dist plscp && cp plscp ../dist - r')
+    os.system('cd playlist-compare && mv dist plscp && cp plscp ../dist -r')
     for f in files:
         shutil.move(f, 'dist')
     break
