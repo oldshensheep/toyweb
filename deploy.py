aa58d897e73fd72713f999e989def6f0924a9dd8
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2022-02-22 11:30:42 +0800

    use html instead

--- deploy.py
+++ deploy.py
@@ -1,15 +1,13 @@
 import os
 import shutil
 
-exclude_dirs = ['playlist-compare', '.github',
+exclude_dirs = ['.github',
                 '.git', '.idea', 'deploy.py', '.gitignore']
 
 for root, dirs, files in os.walk('./'):
     os.mkdir('dist')
     files = set(dirs+files) - set(exclude_dirs)
     print(files)
-    os.system('cd playlist-compare && yarn install && yarn run build')
-    os.system('cd playlist-compare && mv dist plscp && mv plscp ../dist')
     for f in files:
         shutil.move(f, 'dist')
     break
